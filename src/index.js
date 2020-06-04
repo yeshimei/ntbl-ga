732205c1a4163b965dd841d0436076f669beaac3
Sunny <hsy.ntbl@gmail.com>
2020-06-04 22:18:04 +0800

    feat: add public data object store

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ function Ga(options) {
   if (!(this instanceof Ga)) new Ga(options)
 
   this.$router = options.router(this) || {}
-
+  // 公共数据
+  this.store = {}
   initRouter(this)
   // 挂载路由
   this.$mount()
